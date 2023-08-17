72f8d89c6c81ececb452d107eab61e9961eefb5a
Arroyojava <94626607+Arroyojava@users.noreply.github.com>
2023-08-17 14:47:20 -0400

    added labels, entry, and functions

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press F11 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+window = Tk()
+window.title('M to Km Converter')
+window.config(padx=20, pady=20)
+window.minsize(width=275, height=0)
+window.eval('tk::PlaceWindow . Center')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# function to convert miles to km
+def convert(miles):
+    return miles * 1.60934
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def calculate_button_click():
+    if miles_input.get() == '':
+        output_label['text'] = 'Error'
+    else:
+        output_label['text'] = convert(float(miles_input.get()))
+
+
+font = ('Calibri', 12)
+
+# Entry box
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=0)
+
+# Labels
+miles_label = Label(text='Miles', font=font)
+miles_label.grid(column=2, row=0)
+
+equals_label = Label(text='is equal to ', font=font)
+equals_label.grid(column=0, row=1)
+
+output_label = Label(text=0, font=font)
+output_label.grid(column=1, row=1)
+
+km_label = Label(text='Km', font=font)
+km_label.grid(column=2, row=1)
+
+# Button
+calculate_button = Button(text='Calculate', command=calculate_button_click)
+calculate_button.grid(column=1, row=2)
+
+window.mainloop()
